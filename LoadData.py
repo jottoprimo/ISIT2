5870f2ba4a4d091e0bbdb47ee8170cb196518335
Evgenij Katunov <evgenijkatunov@gmail.com>
2016-11-19 10:45:15 +0700

    Add precondition step

--- LoadData.py
+++ LoadData.py
@@ -1,13 +1,25 @@
-from functools import reduce
-
-from numpy import vstack, hstack, column_stack, asarray
+from nltk import RegexpTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import *
+from numpy import asarray, row_stack
 from pandas import read_csv
+from sklearn import metrics
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
-from sklearn import metrics
-import nltk
 
+stop_words = stopwords.words('russian')
+
+
+def precond(message):
+    tokenizer = RegexpTokenizer(r'\w+')
+    stemmer = snowball.RussianStemmer()
+    tokens = tokenizer.tokenize(message.lower())
+    tokens = [stemmer.stem(x) for x in tokens if x not in stop_words]
+    res = ''
+    for t in tokens:
+        res += t+' '
+    return res
 
 
 def load_twitter_msgs():
@@ -15,8 +27,11 @@ def load_twitter_msgs():
              'tfav', 'tstcount', 'tfol', 'tfrien', 'listcount', 'basename']
     data_negative = read_csv('./data/twitter/negative.csv', delimiter=';', names=names)
     data_positive = read_csv('./data/twitter/positive.csv', delimiter=';', names=names)
-    data = vstack((data_negative[['ttext','ttype']], data_positive[['ttext','ttype']]))
-    return data
+    data = row_stack((data_negative[['ttext']], data_positive[['ttext']])).ravel()
+    values = row_stack((data_negative[['ttype']], data_positive[['ttype']])).ravel()
+    values = [v==1 for v in values]
+    data  = asarray(list(map(precond, data)))
+    return data, values
 
 
 def get_bag_of_words(data, max_features):
@@ -25,17 +40,13 @@ def get_bag_of_words(data, max_features):
                                  preprocessor=None,
                                  stop_words=None,
                                  max_features=max_features)
-    bag_of_words = vectorizer.fit_transform(data[:, 0]).toarray()
-    return bag_of_words, data[:, 1]
-    #result = column_stack((bag_of_words, data[:, 1]))
-    #result = [(bag_of_words[x], data[x][1]) for x in range(0, bag_of_words.shape[0])]
-    #print(result)
-    #return result
+    bag_of_words = vectorizer.fit_transform(data).toarray()
+    return bag_of_words
 
 
 def get_sequences(data, values):
     return train_test_split(data, values, train_size=0.7, test_size=0.3)
-    #return train_test_split(data, values, train_size=0.7, test_size=0.3)
+
 
 def classification(train_x, train_y, method=BernoulliNB):
     clf = method()
@@ -46,11 +57,12 @@ def test(clf, test_x, test_y):
     res = clf.predict(test_x)
     return metrics.accuracy_score(test_y, res)
 
-data = load_twitter_msgs()
-bag_of_words, values = get_bag_of_words(data, 7000)
+data, values = load_twitter_msgs()
+bag_of_words = get_bag_of_words(data, 3000)
 x_train, x_test, y_train, y_test = get_sequences(bag_of_words, values)
-y_train = asarray(y_train, dtype=int)
-y_test = asarray(y_test, dtype=int)
-clf = classification(x_train, y_train, MultinomialNB)
-res = test(clf, asarray(x_test,dtype=int), asarray(y_test,dtype=int))
+y_train = asarray(y_train, dtype=bool)
+print(y_train)
+y_test = asarray(y_test, dtype=bool)
+clf = classification(x_train, y_train,MultinomialNB)
+res = test(clf, x_test, y_test)
 print(res)
